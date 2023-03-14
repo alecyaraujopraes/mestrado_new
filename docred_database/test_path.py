bb8e84188281605c4fa01861dbb2f0d10faaf9e8
Alecy Aparecida Araujo Praes <alecy_praes@poli.ufrj.br>
2023-03-14 09:22:00 -0300

    Fix find path

--- docred_database/test_path.py
+++ docred_database/test_path.py
@@ -1,7 +1,7 @@
 dict_dependencies = {
     'Zest Airways , Inc.': [], 
     'operated': ['Zest Airways , Inc.', 'as'], 
-    'as': ['AirAsia Zest'], 
+    'as': ['AirAsia Zest'],
     'AirAsia Zest': ['(', 'formerly Asian Spirit', ')'], 
     '(': [], 
     'formerly Asian Spirit': ['and', 'Zest Air'], 
@@ -22,27 +22,59 @@ dict_dependencies = {
 }
 
 def l(dependencies: dict)-> list:
-
-    def build_path(l:list, v:list)->list:
+    paths_list = []
+    def build_path(lb:list, v:list)->list:
+        idx_v = 0
+        idx_dep = 0
         for item in v:
-            l.append(item)
+            idx_v+=1
+            if idx_v!=1:
+                lb = lb[0:idx_v]
+            lb.append(item)
             if item in dependencies:
+                idx_dep+=1
+                if idx_dep!=1:
+                    lb = lb[0:idx_dep]
                 v = dependencies.get(f"{item}")
                 if v:
-                    build_path(l, v)
+                    build_path(lb, v)
                 else:
-                    return l
-
+                    paths_list.append(lb)
+                    # size = len(lb)
+                    # lb = lb[0:size-1]
 
     for k, v in dependencies.items():
         l = []
-        l.append(k)
         build_path(l, v)
-        print(l)
+    return paths_list
 
-l(dict_dependencies)
-        
+
     
+def find_paths (dict_dependencies: dict, paths: list)->list:
+    if len(paths)==0:
+        for k,v in dict_dependencies.items():
+            for item in v:
+                paths.append([k, item])
+        find_paths(dict_dependencies, paths)
+        print(f"Paths first: {paths}")
+    else:
+        print(f"Paths: {paths}")
+        for item in paths:
+            print(f"Paths: {item}")
+            new_search = item[len(item)-1]
+            if new_search in dict_dependencies:
+                for i in new_search:
+                    new_item = item.append(i)
+                    paths.append(new_item)
+            else:
+                return paths
+        find_paths(dict_dependencies, paths)
+    
+paths_list = []
+p = find_paths(dict_dependencies, paths_list)
+print(p)
     
 
 
+# pl = l(dict_dependencies)
+# print(pl)
