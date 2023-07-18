a5d81c79a25561654a9fd30f8b8c75d2d7787bfd
Alecy Aparecida Araujo Praes <alecy_praes@poli.ufrj.br>
2023-07-18 00:18:57 -0300

    Add jaro wnkler functions to utils

--- docred_database/compare_bert_annotations.py
+++ docred_database/compare_bert_annotations.py
@@ -26,10 +26,16 @@ for index, row in df.iterrows():
     # for sentence in list_sentences:
     #     print(f"Sentence: {sentence}")
     list_entities = find_entities(sentences)
+    print(f"List entities: {list_entities}")
     dict_dependencies = get_dict_dependencies(sentences)
+    print(f"Dict dependencies: {dict_dependencies}")
     list_nodes = find_nodes(sentences)
+    print(f"List nodes: {list_nodes}")
     dict_nodes = get_nodes_entities(list_entities, list_nodes)
+    print(f"Dict nodes: {dict_nodes}")
     combination_noun_phrases = combination_between_noun_phrases(list_entities)
+    print(f"Combination noun phrases: {combination_noun_phrases}")
+
 
     nodes = {}
     for n in [Node(k) for k,v in dict_dependencies.items()]: nodes[n.name] = n
@@ -37,7 +43,9 @@ for index, row in df.iterrows():
         children = dict_dependencies[n.name]
         n.set_children([nodes[c] for c in children])
 
+
     for tuple_nodes in combination_noun_phrases:
+        print(f"Tuple nodes: {tuple_nodes}")
         entity_0 = dict_nodes.get(f"{tuple_nodes[0]}")
         entity_1 = dict_nodes.get(f"{tuple_nodes[1]}")
         tuple_of_entities = (entity_0, entity_1)
@@ -57,46 +65,43 @@ for index, row in df.iterrows():
 
         if relation_found_by_path and entity_0 != entity_1:
             print(f"Entities: {tuple_of_entities}")
-            relation_id_found_by_bert, relation_found_by_bert = selection_by_bert(entity_0, entity_1, relation_found_by_path)
+            # relation_id_found_by_bert, relation_found_by_bert = selection_by_bert(entity_0, entity_1, relation_found_by_path)
 
             for entity_tail, entity_head, relation_annotated, code_relation in zip(df_docred_selected.entity_tail, df_docred_selected.entity_head, df_docred_selected.relation, df_docred_selected.code_relation):
                 print(f"Tail and head entities: {entity_head, entity_tail}")
-                if (jaro_winkler(entity_tail, entity_0)> 0.7 or jaro_winkler(entity_tail, entity_1)>0.7) and (jaro_winkler(entity_head, entity_0)> 0.7 or jaro_winkler(entity_head, entity_1)>0.7):
-                    print(f"Similar entities")
-                    print(f"Entidades: {entity_tail, entity_head, entity_0, entity_1}")
+            #     if (jaro_winkler(entity_tail, entity_0)> 0.7 or jaro_winkler(entity_tail, entity_1)>0.7) and (jaro_winkler(entity_head, entity_0)> 0.7 or jaro_winkler(entity_head, entity_1)>0.7):
+            #         print(f"Similar entities")
+            #         print(f"Entidades: {entity_tail, entity_head, entity_0, entity_1}")
 
-                    if code_relation == relation_id_found_by_bert:
-                        result = 1
+            #         if code_relation == relation_id_found_by_bert:
+            #             result = 1
                 
-                else:
-                    entity_head = ""
-                    entity_tail = ""
 
-                field_names = [
-                    "sentences", 
-                    "entity_0", 
-                    "entity_1",
-                    "entity_tail",
-                    "entity_head",
-                    "relation_found_by_path",
-                    "relation_found_by_bert",
-                    "relation_annotated",
-                    "result",
-                ]
+                # field_names = [
+                #     "sentences", 
+                #     "entity_0", 
+                #     "entity_1",
+                #     "entity_tail",
+                #     "entity_head",
+                #     "relation_found_by_path",
+                #     "relation_found_by_bert",
+                #     "relation_annotated",
+                #     "result",
+                # ]
 
-                with open('docred_database/check_bert_and_annotations.csv', 'a') as f_object:
-                    dictwriter_object = csv.DictWriter(f_object, fieldnames=field_names)
-                    writer = csv.DictWriter(f_object, fieldnames=field_names)
-                    writer.writerow({
-                        "sentences": sentences, 
-                        "entity_0": entity_0, 
-                        "entity_1": entity_1,
-                        "entity_tail": entity_tail,
-                        "entity_head": entity_head,
-                        "relation_found_by_path": relation_found_by_path,
-                        "relation_found_by_bert": relation_found_by_bert,
-                        "relation_annotated": relation_annotated,
-                        "result": result,
-                    })
+                # with open('docred_database/check_bert_and_annotations.csv', 'a') as f_object:
+                #     dictwriter_object = csv.DictWriter(f_object, fieldnames=field_names)
+                #     writer = csv.DictWriter(f_object, fieldnames=field_names)
+                #     writer.writerow({
+                #         "sentences": sentences, 
+                #         "entity_0": entity_0, 
+                #         "entity_1": entity_1,
+                #         "entity_tail": entity_tail,
+                #         "entity_head": entity_head,
+                #         "relation_found_by_path": relation_found_by_path,
+                #         "relation_found_by_bert": relation_found_by_bert,
+                #         "relation_annotated": relation_annotated,
+                #         "result": result,
+                #     })
 
-                    f_object.close()
+                #     f_object.close()
--- docred_database/utils.py
+++ docred_database/utils.py
@@ -1,22 +1,31 @@
 from Levenshtein import jaro_winkler
 
 
-def compare_strings(list_entities: list)-> list:
-    similar_entities = []
+def compare_strings(ent_o, ent_h)-> bool:
 
-    for ent in list_entities:
-        list_ent = []
-        for ent0 in list_entities:
-            if jaro_winkler(ent, ent0)> 0.65 and jaro_winkler(ent, ent0) < 1:
-                list_ent.append(ent0)
-                print(jaro_winkler(ent, ent0))
-                print(f"Same entities")
-            else:
-                print(jaro_winkler(ent, ent0))
-                print(f"Different entities")
-        ent_similar = {ent: list_ent}
-        similar_entities.append(ent_similar)
+    if jaro_winkler(ent_o, ent_h)> 0.7 and jaro_winkler(ent_o, ent_h) < 1:
+        similar_ent = 1
+        print(f"Similar entities")
+    else:
+        similar_ent = 0
+        print(jaro_winkler(ent_o, ent_h))
+        print(f"Different entities")
     
-    print(similar_entities)
+    print(similar_ent)
 
-    return similar_entities
+    return similar_ent
+
+
+def get_most_similar_pair_entities(df_selected, entity_0, entity_1):
+    pairs_and_similarities = {}
+
+    for entity_tail, entity_head in zip(df_selected.entity_tail, df_selected.entity_head):
+        tail_jw = jaro_winkler(entity_tail, entity_0)
+        head_jw = jaro_winkler(entity_head, entity_1)
+        avg_jw = (tail_jw + head_jw)/2
+        pairs_and_similarities[f"{entity_tail}+{entity_head}"] = avg_jw
+
+    max_jw_pair = max(pairs_and_similarities, key=pairs_and_similarities.get)
+    max_jw_value = max(pairs_and_similarities.values())
+
+    return max_jw_pair, max_jw_value
