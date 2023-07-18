41bf6f6fe47430547f7f39e5f11bfa4f57af680f
Alecy Aparecida Araujo Praes <alecy_praes@poli.ufrj.br>
2023-07-18 09:36:10 -0300

    Fix util functions

--- docred_database/compare_bert_annotations.py
+++ docred_database/compare_bert_annotations.py
@@ -7,6 +7,7 @@ from ParseSyntaticTree import (Node, combination_between_noun_phrases,
                                find_entities, find_nodes,
                                get_dict_dependencies, get_nodes_entities,
                                split_in_sentences)
+from utils import compare_strings, get_most_similar_pair_entities
 
 df_docred = pd.read_csv("docred_database/docred.csv")
 
@@ -26,15 +27,15 @@ for index, row in df.iterrows():
     # for sentence in list_sentences:
     #     print(f"Sentence: {sentence}")
     list_entities = find_entities(sentences)
-    print(f"List entities: {list_entities}")
+    # print(f"List entities: {list_entities}")
     dict_dependencies = get_dict_dependencies(sentences)
-    print(f"Dict dependencies: {dict_dependencies}")
+    # print(f"Dict dependencies: {dict_dependencies}")
     list_nodes = find_nodes(sentences)
-    print(f"List nodes: {list_nodes}")
+    # print(f"List nodes: {list_nodes}")
     dict_nodes = get_nodes_entities(list_entities, list_nodes)
-    print(f"Dict nodes: {dict_nodes}")
+    # print(f"Dict nodes: {dict_nodes}")
     combination_noun_phrases = combination_between_noun_phrases(list_entities)
-    print(f"Combination noun phrases: {combination_noun_phrases}")
+    # print(f"Combination noun phrases: {combination_noun_phrases}")
 
 
     nodes = {}
@@ -45,7 +46,6 @@ for index, row in df.iterrows():
 
 
     for tuple_nodes in combination_noun_phrases:
-        print(f"Tuple nodes: {tuple_nodes}")
         entity_0 = dict_nodes.get(f"{tuple_nodes[0]}")
         entity_1 = dict_nodes.get(f"{tuple_nodes[1]}")
         tuple_of_entities = (entity_0, entity_1)
@@ -61,20 +61,20 @@ for index, row in df.iterrows():
             path.append(n.name)
 
         relation_found_by_path = " ".join(path)
-        print(f"Relation found by path: {relation_found_by_path}")
 
         if relation_found_by_path and entity_0 != entity_1:
+            print(f"Relation found by path: {relation_found_by_path}")
             print(f"Entities: {tuple_of_entities}")
             # relation_id_found_by_bert, relation_found_by_bert = selection_by_bert(entity_0, entity_1, relation_found_by_path)
 
-            for entity_tail, entity_head, relation_annotated, code_relation in zip(df_docred_selected.entity_tail, df_docred_selected.entity_head, df_docred_selected.relation, df_docred_selected.code_relation):
-                print(f"Tail and head entities: {entity_head, entity_tail}")
-            #     if (jaro_winkler(entity_tail, entity_0)> 0.7 or jaro_winkler(entity_tail, entity_1)>0.7) and (jaro_winkler(entity_head, entity_0)> 0.7 or jaro_winkler(entity_head, entity_1)>0.7):
-            #         print(f"Similar entities")
-            #         print(f"Entidades: {entity_tail, entity_head, entity_0, entity_1}")
+            list_ents_similars, jw_factor = get_most_similar_pair_entities(df_docred_selected, entity_0, entity_1)
+            print(f"SImilares pairs: {entity_0, entity_1}, {list_ents_similars}")
+            similar_ents = False
 
-            #         if code_relation == relation_id_found_by_bert:
-            #             result = 1
+            for entity_tail, entity_head, relation_annotated, code_relation in zip(df_docred_selected.entity_tail, df_docred_selected.entity_head, df_docred_selected.relation, df_docred_selected.code_relation):
+                if compare_strings(entity_tail, entity_0) and compare_strings(entity_head, entity_1):
+                    similar_ents = True
+            
                 
 
                 # field_names = [
--- docred_database/utils.py
+++ docred_database/utils.py
@@ -3,15 +3,12 @@ from Levenshtein import jaro_winkler
 
 def compare_strings(ent_o, ent_h)-> bool:
 
-    if jaro_winkler(ent_o, ent_h)> 0.7 and jaro_winkler(ent_o, ent_h) < 1:
+    if jaro_winkler(ent_o, ent_h)> 0.7 and jaro_winkler(ent_o, ent_h) != 1:
         similar_ent = 1
         print(f"Similar entities")
     else:
         similar_ent = 0
-        print(jaro_winkler(ent_o, ent_h))
         print(f"Different entities")
-    
-    print(similar_ent)
 
     return similar_ent
 
@@ -26,6 +23,7 @@ def get_most_similar_pair_entities(df_selected, entity_0, entity_1):
         pairs_and_similarities[f"{entity_tail}+{entity_head}"] = avg_jw
 
     max_jw_pair = max(pairs_and_similarities, key=pairs_and_similarities.get)
+    pair_ents = max_jw_pair.split("+")
     max_jw_value = max(pairs_and_similarities.values())
 
-    return max_jw_pair, max_jw_value
+    return pair_ents, max_jw_value
